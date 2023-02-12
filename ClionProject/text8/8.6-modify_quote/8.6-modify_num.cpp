78c1aaccd6e8d1b3474fbfb4ca4f31a2b87804bc
xiaofeicoding <gongda2021910220@gmail.com>
2023-02-12 23:17:45 +0800

    finsh

--- ClionProject/text8/8.6-modify_quote/8.6-modify_num.cpp
+++ ClionProject/text8/8.6-modify_quote/8.6-modify_num.cpp
@@ -17,15 +17,15 @@
 ////E:\WangDao_C\ClionProject\text8\8.6-modify_quote\cmake-build-debug\8.6-modify_num.exe
 ////
 ////进程已结束,退出代码-1073741819 (0xC0000005)
-////输出结果是-1073741819 为负数就说名程序异常输出
+////输出结果是-1073741819 不为0，那么代表进程异常结束
 
 #include <iostream>
 
-void modify_pointer(int* &p,int *q)
+void modify_pointer(int* &p,int *q)//引用&必须和变量名紧邻
 {
     p=q;
 }
-
+//子函数内修改主函数的一级指针变量
 int main()
 {
     int *p=NULL;
